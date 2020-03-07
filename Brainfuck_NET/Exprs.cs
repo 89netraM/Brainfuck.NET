d5ae04e56404d64f8f5c230abcc3b3b5a977b9e4
Mårten Åsberg <marten.asberg@outlook.com>
2020-03-07 12:19:17 +0100

    Upgraded methods protection level

--- Brainfuck_NET/Exprs.cs
+++ Brainfuck_NET/Exprs.cs
@@ -13,7 +13,7 @@ namespace Brainfuck_NET
 		private const string inputName = "input";
 		private const string enumeratorName = "enumerator";
 
-		static IEnumerable<StatementSyntax> MethodInit(IOKind ioKind)
+		internal static IEnumerable<StatementSyntax> MethodInit(IOKind ioKind)
 		{
 			yield return SyntaxFactory.ParseStatement($"int {pointerName} = 0;");
 			yield return SyntaxFactory.ParseStatement($"byte[] {arrayName} = new byte[{arraySize}];");
@@ -24,31 +24,31 @@ namespace Brainfuck_NET
 			}
 		}
 
-		static StatementSyntax Shift(int steps)
+		internal static StatementSyntax Shift(int steps)
 		{
 			return SyntaxFactory.ParseStatement($"{pointerName} += {steps};");
 		}
 
-		static StatementSyntax Increment(int change)
+		internal static StatementSyntax Increment(int change)
 		{
 			return SyntaxFactory.ParseStatement($"{arrayName}[{pointerName}] = (byte)(({arrayName}[{pointerName}] + {change}) % 256);");
 		}
 
-		static StatementSyntax Input(IOKind ioKind) => ioKind switch
+		internal static StatementSyntax Input(IOKind ioKind) => ioKind switch
 		{
 			IOKind.Argument => SyntaxFactory.ParseStatement($"{arrayName}[{pointerName}] = {enumeratorName}.MoveNext() ? {enumeratorName}.Current : (byte)0;"),
 			IOKind.Console => SyntaxFactory.ParseStatement($"{arrayName}[{pointerName}] = (byte)Console.Read();"),
 			_ => throw new ArgumentException($"Must have valid {nameof(IOKind)}", nameof(ioKind))
 		};
 
-		static StatementSyntax Output(IOKind ioKind) => ioKind switch
+		internal static StatementSyntax Output(IOKind ioKind) => ioKind switch
 		{
 			IOKind.Argument => SyntaxFactory.ParseStatement($"yield return {arrayName}[{pointerName}];"),
 			IOKind.Console => SyntaxFactory.ParseStatement($"Console.Write((char){arrayName}[{pointerName}]);"),
 			_ => throw new ArgumentException($"Must have valid {nameof(IOKind)}", nameof(ioKind))
 		};
 
-		static StatementSyntax Loop(IEnumerable<StatementSyntax> innerStatements)
+		internal static StatementSyntax Loop(IEnumerable<StatementSyntax> innerStatements)
 		{
 			return SyntaxFactory.WhileStatement(SyntaxFactory.ParseExpression($"{arrayName}[{pointerName}] != (byte)0"), SyntaxFactory.Block(innerStatements));
 		}
